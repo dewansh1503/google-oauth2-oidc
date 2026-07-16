aee0a71a6e71340d6ae7414f2e873d9193bf52a4
Dewansh Pandey <dewansh1503@gmail.com>
2026-07-16 07:58:19 +0530

    made express app

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import { OAuth2Client } from 'google-auth-library';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import crypto from 'crypto';
 import { createClient } from 'redis';
+
+const app = express();
+dotenv.config();
+app.use(cors({ origin: 'http://localhost:4000', credentials: true }));
+app.use(cookieParser());
+
+app.listen(3000, () => {
+	console.log('listening');
+});
