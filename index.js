62c158cab83dcd4b9fc9eea678cb72399146a927
Dewansh Pandey <dewansh1503@gmail.com>
2026-07-24 22:16:25 +0530

    importing api error and error handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { OAuth2Client } from 'google-auth-library';
+import { apiError, errorhandler } from './utils.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import crypto from 'crypto';
