e7e1b4d8012d1ba3aaf764b35665aa7c3575f5b6
Dewansh Pandey <dewansh1503@gmail.com>
2026-08-02 14:26:58 +0530

    refactor: remove Redis and PostgreSQL connection checks from index.js

--- index.js
+++ index.js
@@ -12,39 +12,6 @@ dotenv.config();
 app.use(cors({ origin: 'http://localhost:4000', credentials: true }));
 app.use(cookieParser());
 
-const redisClient = createClient();
-async function checkRedis() {
-	try {
-		await redisClient.connect();
-		await redisClient.ping();
-		console.log('Redis connected');
-	} catch (err) {
-		throw new apiError(503, `Redis: ${err.message}`);
-	}
-}
-checkRedis();
-
-const pool = new Pool({
-	host: 'localhost',
-	port: process.env.PSQL_PORT,
-	user: process.env.PSQL_USER,
-	password: process.env.PSQL_PASSWORD,
-	database: process.env.PSQL_DATABASE,
-	// error will be thrown after 10sec if not able to connect to db
-	connectionTimeoutMillis: 7000,
-});
-
-async function checkDB() {
-	try {
-		const client = await pool.connect();
-		console.log('PostgreSQL connected');
-		client.release();
-	} catch (err) {
-		throw new apiError(503, `PostgreSQL ${err.message}`);
-	}
-}
-checkDB();
-
 function random(encoding = 'base64url', size = 32) {
 	return crypto.randomBytes(size).toString(encoding);
 }
